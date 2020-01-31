8f0940fc2a2d01dc1670b651c5dca7455949344a
Mike Douglas <michael.j.douglas@gmail.com>
2020-01-31 06:55:00 -0500

    Setting up github pages

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tsm-editor/'
+    : '/',
   css: {
     loaderOptions: {
       scss: {
-        prependData: `@import "~@/assets/scss/_variables.scss";`,
+        prependData: '@import "~@/assets/scss/_variables.scss";',
       },
     },
   },
