5dc22bc91d1341d6082065f6d02c583a2048342d
Mike Douglas <michael.j.douglas@gmail.com>
2020-01-31 07:18:58 -0500

    Adding gtag

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 import App from './App.vue';
 import store from '@/lib/store';
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-49351028-1' },
+});
 
 Vue.config.productionTip = false;
 
