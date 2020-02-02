9c36bc9ee8691a3083adb7d5373f96c5c1b56e44
Mike Douglas <michael.j.douglas@gmail.com>
2020-02-02 15:56:57 -0500

    Adding new build with bugsnag

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import VueGtag from 'vue-gtag';
+import bugsnag from '@bugsnag/js';
+import bugsnagVue from '@bugsnag/plugin-vue';
+
 import App from './App.vue';
 import store from '@/lib/store';
+
+const bugsnagClient = bugsnag({
+  apiKey: '1a5d9bde039258ef72cf342d7057f654',
+  appVersion: process.env.PACKAGE_VERSION,
+});
+
+bugsnagClient.use(bugsnagVue, Vue);
 
 Vue.use(VueGtag, {
   config: { id: 'UA-49351028-1' },
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const packageJson = fs.readFileSync('./package.json');
+const version = JSON.parse(packageJson).version || 0;
+const webpack = require('webpack');
+const { BugsnagSourceMapUploaderPlugin } = require('webpack-bugsnag-plugins');
+
 module.exports = {
   outputDir: 'docs/',
   publicPath: process.env.NODE_ENV === 'production'
@@ -9,5 +15,25 @@ module.exports = {
         prependData: '@import "~@/assets/scss/_variables.scss";',
       },
     },
+  },
+  configureWebpack: () => {
+    const plugins = [
+      new webpack.DefinePlugin({
+        'process.env': {
+          PACKAGE_VERSION: `"${version}"`,
+        },
+      }),
+    ];
+
+    if (process.env.NODE_ENV === 'production') {
+      plugins.push(new BugsnagSourceMapUploaderPlugin({
+        apiKey: '1a5d9bde039258ef72cf342d7057f654',
+        appVersion: `${version}`,
+      }));
+
+      return { devtool: 'source-maps', plugins };
+    }
+
+    return { plugins };
   },
 };
