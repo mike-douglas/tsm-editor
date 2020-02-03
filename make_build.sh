c55e8838ea395221ab021b3a2fc16f6877e44432
Mike Douglas <michael.j.douglas@gmail.com>
2020-02-03 08:03:17 -0500

    Fixing bugs with build script

--- make_build.sh
+++ make_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(git status -s | wc -l) > 0 ]; then
+if [ $(git status -s | wc -l) -gt 0 ]; then
   echo "You have uncommitted changes. Please commit or stash your changes before continuing"
   exit
 fi
@@ -10,7 +10,7 @@ CURRENT_VERSION=$(grep \"version\" package.json | awk -F'"' '{print $4}')
 read -p "What version (current is ${CURRENT_VERSION})? " NEXT_VERSION
 echo "Building v${NEXT_VERSION}..."
 
-perl -pi -e "s/\"version\": \"${CURRENT_VERSION}\"/\"version: \"${NEXT_VERSION}\"/" package.json && \
+perl -pi -e "s/\"version\": \"${CURRENT_VERSION}\"/\"version\": \"${NEXT_VERSION}\"/" package.json && \
   yarn build && \
   git st && \
   git add ./ && \
