3af046c91f6c9b963b2c4b504d92dec7019634da
Mike Douglas <michael.j.douglas@gmail.com>
2020-09-11 14:51:04 -0400

    Introducing language parser for TSM Strings
    This work replaces the old regex-based parser to support real language
    evaluation in the future to support some new features.
    
    - New Lexer and Parser classes that implement tokenization and contextual
      parsing, respectively.
    - Parser returns an AST that can be used to evaluate the nodes within,
      according to the rules of the TSM string language.
    - reformatString and stylizeString use the new Lexer to style input.
    - Tests rewritten for those functions, as well as a suite for the Lexer and
      Parser.

--- tests/unit/lib/stylizer.spec.js
+++ tests/unit/lib/stylizer.spec.js
@@ -2,6 +2,55 @@
 import { reformatter, stylizeString } from '@/lib/stylizer.js';
 import { symbols, functions } from '@/lib/definitions.js';
 
+describe('stylizeString', () => {
+  it('correctly tags percent strings', () => {
+    expect(stylizeString('200% DBMarket')).toMatch('<span class="token numeric">200</span><span class="token percent">%</span> <span class="token symbol">DBMarket</span>');
+  });
+
+  it('correctly tags math operations', () => {
+    ['+', '-', '*', '/'].forEach((operation) => {
+      expect(stylizeString(`2 ${operation} 2`)).toMatch(`<span class="token numeric">2</span> <span class="token maths">${operation}</span> <span class="token numeric">2</span>`);
+    });
+  });
+
+  it('correctly tags item strings', () => {
+    expect(stylizeString('[Foo of Bar]')).toMatch('<span class="token brackets lbracket">[</span><span class="token ">Foo</span> <span class="token ">of</span> <span class="token ">Bar</span><span class="token brackets rbracket">]</span>');
+    expect(stylizeString('i:31337')).toMatch('<span class="token ">i</span><span class="token punc">:</span><span class="token numeric">31337</span>');
+    expect(stylizeString('item:31337')).toMatch('<span class="token ">item</span><span class="token punc">:</span><span class="token numeric">31337</span>');
+  });
+
+  it('correctly tags currency strings', () => {
+    const currency = {
+      '200g': '<span class="token currency gold"><span class="token numeric">200</span>g</span>',
+      '30s': '<span class="token currency silver"><span class="token numeric">30</span>s</span>',
+      '10c': '<span class="token currency copper"><span class="token numeric">10</span>c</span>',
+      '523g44s1c': '<span class="token currency gold"><span class="token numeric">523</span>g</span><span class="token currency silver"><span class="token numeric">44</span>s</span><span class="token currency copper"><span class="token numeric">1</span>c</span>',
+    };
+
+    Object.keys(currency).forEach((key) => {
+      expect(stylizeString(key)).toMatch(currency[key]);
+    });
+  });
+
+  it('correctly tags each defined function string', () => {
+    functions.forEach((func) => {
+      expect(stylizeString(`${func.name}(10,20)`)).toContain('class="token function"');
+      expect(stylizeString(`${func.name} ()`)).toContain('class="token function"');
+      expect(stylizeString(`${func.name}(DBMarket, 200g)`)).toContain('class="token function"');
+    });
+  });
+
+  it('correctly tags each defined symbol string', () => {
+    symbols.forEach((symbol) => {
+      expect(stylizeString(symbol.name)).toContain('class="token symbol"');
+    });
+  });
+
+  it('correctly tags numeric strings', () => {
+    expect(stylizeString('1234231')).toMatch('<span class="token numeric">1234231</span>');
+  });
+});
+
 describe('reformatter', () => {
   it('adds a single space after commas', () => {
     const text = 'first(Crafting,DBMarket,DBRegionMarketAvg)';
@@ -20,68 +69,11 @@ describe('reformatter', () => {
     expect(reformatter('MAX()')).toMatch('max()');
   });
 
-  it('pads item links with a space WITHOUT adding spaces inside the brackets', () => {
-    expect(reformatter('[Bag of Tricks  with extra space there]')).toMatch(' [Bag of Tricks  with extra space there] ');
+  it('item link spaces are not changed inside brackets', () => {
+    expect(reformatter('[Bag of Tricks  with extra space there]')).toMatch('[Bag of Tricks  with extra space there]');
   });
 
   it('does not add a space between a value and %', () => {
     expect(reformatter('200% DBMarket')).toEqual('200% DBMarket');
-  });
-});
-
-describe('stylizeString', () => {
-  it('correctly tags math operations', () => {
-    '+-*/'.split('').forEach((operation) => {
-      expect(stylizeString(`2 ${operation} 2`)).toMatch(`<span class="token numeric">2</span> <span class="token maths">${operation}</span> <span class="token numeric">2</span>`);
-    });
-  });
-
-  it('correctly handles percent strings', () => {
-    expect(stylizeString('200% DBMarket')).toMatch('<span class="token numeric">200</span><span class="token percent">%</span> <span class="token symbol">DBMarket</span>');
-  });
-
-  it('correctly tags item strings', () => {
-    expect(stylizeString('[Foo of Bar]')).toMatch('<span class="token item">[Foo of Bar]</span>');
-    expect(stylizeString('i:31337')).toMatch('<span class="token item">i:<span class="token numeric">31337</span></span>');
-    expect(stylizeString('item:31337')).toMatch('<span class="token item">item:<span class="token numeric">31337</span></span>');
-  });
-
-  it('correctly tags currency strings', () => {
-    const currency = {
-      '200g': '<span class="token currency gold"><span class="token numeric">200</span>g</span>',
-      '30s': '<span class="token currency silver"><span class="token numeric">30</span>s</span>',
-      '10c': '<span class="token currency copper"><span class="token numeric">10</span>c</span>',
-      '523g44s1c': '<span class="token currency gold"><span class="token numeric">523</span>g</span><span class="token currency silver"><span class="token numeric">44</span>s</span><span class="token currency copper"><span class="token numeric">1</span>c</span>',
-    };
-
-    Object.keys(currency).forEach((key) => {
-      expect(stylizeString(key)).toMatch(currency[key]);
-    });
-  });
-
-  it('correctly tags each defined function string', () => {
-    functions.forEach((func) => {
-      expect(stylizeString(`${func.name}()`)).toContain('class="token function"');
-      expect(stylizeString(`${func.name} ()`)).toContain('class="token function"');
-    });
-  });
-
-  it('correctly tags each defined symbol string', () => {
-    symbols.forEach((symbol) => {
-      expect(stylizeString(symbol.name)).toContain('class="token symbol"');
-    });
-  });
-
-  it('correctly tags parentheses strings', () => {
-    expect(stylizeString('(')).toMatch('<span class="token parens lparen">(</span>');
-    expect(stylizeString(')')).toMatch('<span class="token parens rparen">)</span>');
-  });
-
-  it('correctly tags punctuation', () => {
-    expect(stylizeString(',')).toMatch('<span class="token punc">,</span>');
-  });
-
-  it('correctly tags numeric strings', () => {
-    expect(stylizeString('1234231')).toMatch('<span class="token numeric">1234231</span>');
   });
 });
